88408099285b242776d54d4dbcde57cce1082a1e
Елизавета <liza@tune-it.ru>
2021-05-11 15:58:25 +0300

    fix swagger

--- src/main/java/ru/dins/testtask/swagger/SwaggerConfig.java
+++ src/main/java/ru/dins/testtask/swagger/SwaggerConfig.java
@@ -33,6 +33,8 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     private ApiInfo apiInfo() {
